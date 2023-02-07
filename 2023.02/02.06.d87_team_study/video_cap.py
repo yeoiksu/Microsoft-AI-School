58bc04f6cbde047f1ef03c3fbbb97ebac5d1f69d
Iksu Yeo <iksuyeo@gmail.com>
2023-02-07 20:51:58 +0900

    update 23.02.07

--- 2023.02/02.06.d87_team_study/video_cap.py
+++ 2023.02/02.06.d87_team_study/video_cap.py
@@ -3,15 +3,15 @@ video_path = "./2023.02/02.04.d87_team_study/4.mp4"
 
 vidcap = cv2.VideoCapture(video_path) ## 다운받은 비디오 이름 
 success,image = vidcap.read()
-count = 434
+count = 0
 
 while(vidcap.isOpened()):
     ret, image = vidcap.read()
     
-    if count == 800: # 종료 시점 
+    if count == 5000: # 종료 시점 
         break
 
-    if(int(vidcap.get(1)) % 30 == 0): # XX 프레임당 저장
+    if(int(vidcap.get(1)) % 10 == 0): # XX 프레임당 저장
         print('Saved frame number : ' + str(int(vidcap.get(1))))
         cv2.imwrite("./2023.02/02.04.d87_team_study/helicopter/8.video/frame%d.jpg" % count, image)
         print('Saved frame%d.jpg' % count)
